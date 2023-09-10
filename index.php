a0fd49607f2adc2cd4a620dda5c36b693ba79423
aliemtakarai <aliemtakarai@gmail.com>
2023-09-10 22:18:46 +0800

    update comment

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 require_once 'vendor/autoload.php';
-
+/**
+ * this project using ramsey/UUID 
+ * https://uuid.ramsey.dev/en/stable/quickstart.html#install-with-composer
+ */
 use Ramsey\Uuid\Uuid;
 
 $uuid = Uuid::uuid4();
